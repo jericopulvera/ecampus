0110f89b2ecdfe4305c54e7db80ca15795dca9c9
eco <jerico.pulvera01z@gmail.com>
2017-03-14 22:53:56 +0800

    shouldbroadcastnow

--- app/Events/MessageWasCreated.php
+++ app/Events/MessageWasCreated.php
@@ -6,7 +6,7 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Queue\SerializesModels;
 
 class MessageWasCreated implements ShouldBroadcastNow
