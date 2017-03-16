a14aec32b5b70c7aed40edaa5509150812e26083
eco <jerico.pulvera01z@gmail.com>
2017-03-17 06:01:16 +0800

    fix schedule displaying on different academic term

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Setting;
 
 class ScheduleController extends Controller
 {
     public function getSchedule($usn)
     {
         $user = User::where('usn', $usn)->first();
-        $schedule = $user->groups()->get();
+        $term_id = Setting::first()->term_id;
+        $schedule = $user->groups()->where('term_id', $term_id)->get();
 
         return view('schedule.schedule-index', compact('schedule', 'user'));
     }
